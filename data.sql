bed3aab556fc6b5c8ba19806039d633d59c3bfd3
manu <manu.v>
2021-06-04 14:24:07 +0200

    genre, auteur, editeur

---
+++ data.sql
@@ -0,0 +1,8 @@
+INSERT INTO auteur (nom, prenom) VALUES
+('Ollivier', 'Sébastien'),
+('Djordjevic', 'Daniel'),
+('Klein','William');
+
+INSERT INTO editeur (nomEditeur) VALUES ('Jean-Sebastien'), ('Arnaud'), ('ib'), ('Mercure');
+
+INSERT INTO genre (genre) VALUES ('programmation'), ('bureautique'), ('os');
